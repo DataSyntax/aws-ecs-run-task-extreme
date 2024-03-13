7ae6f2d902f571ee7b254efd459e315be3fabdec
ross <1242369+rivaros@users.noreply.github.com>
2024-03-13 21:35:02 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# AWS ECS Run Task
+
+Runs a one-off task on AWS ECS Fargate.
+
+Usage
+``` yaml
+- name: Run migration
+  uses: datasyntax/aws-ecs-run-task-extreme@v1.0
+  with:
+    cluster: staging
+    task-defintion: run_migration_task_def
+    subnets: sb-123123
+    security-groups: sg-1231231
+
+- name: Run more Migrations
+  uses: datasyntax/aws-ecs-run-task-extreme@v1.0
+  with:
+    cluster: staging
+    task-defintion: server_backend_task_def
+    subnets: sb-123123
+    security-groups: sg-1231231
+    assign-public-ip: DISABLED
+    container-override: server
+    container-command: |
+        sh
+        -c
+        cd database && python migrate.py
+```
+
+See [aciton.yml](action.yml) file for the full documentation for this action's inputs and outputs.
+
+Note: the `task-definition` input requires the name of the task-defintion. If you need to use task-definition input files, consider creating other actions to create the task defintion first.  
+
+## Credentials and Region
+
+This action relies on the [default behavior of the AWS SDK for Javascript](https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html) to determine AWS credentials and region.
+Use [the `aws-actions/configure-aws-credentials` action](https://github.com/aws-actions/configure-aws-credentials) to configure the GitHub Actions environment with environment variables containing AWS credentials and your desired region.
+
+## License Summary
+
+This code is made available under the MIT license.
---
+++ index.js
@@ -0,0 +1,91 @@
+const core = require("@actions/core");
+const AWS = require("aws-sdk");
+
+const ecs = new AWS.ECS();
+
+const main = async () => {
+  const cluster = core.getInput("cluster", { required: true });
+  const taskDefinition = core.getInput("task-definition", { required: true });
+  const subnets = core.getMultilineInput("subnets", { required: true });
+  const securityGroups = core.getMultilineInput("security-groups", {
+    required: true,
+  });
+
+  const assignPublicIp =
+    core.getInput("assign-public-ip", { required: false }) || "ENABLED";
+  const overrideContainer = core.getInput("override-container", {
+    required: false,
+  });
+  const overrideContainerCommand = core.getMultilineInput(
+    "override-container-command",
+    {
+      required: false,
+    }
+  );
+
+  const taskParams = {
+    taskDefinition,
+    cluster,
+    count: 1,
+    launchType: "FARGATE",
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        subnets,
+        assignPublicIp,
+        securityGroups,
+      },
+    },
+  };
+
+  try {
+    if (overrideContainerCommand.length > 0 && !overrideContainer) {
+      throw new Error(
+        "override-container is required when override-container-command is set"
+      );
+    }
+
+    if (overrideContainer) {
+      if (overrideContainerCommand) {
+        taskParams.overrides = {
+          containerOverrides: [
+            {
+              name: overrideContainer,
+              command: overrideContainerCommand,
+            },
+          ],
+        };
+      } else {
+        throw new Error(
+          "override-container-command is required when override-container is set"
+        );
+      }
+    }
+
+    core.debug("Running task...");
+    let task = await ecs.runTask(taskParams).promise();
+    const taskArn = task.tasks[0].taskArn;
+    core.setOutput("task-arn", taskArn);
+
+    core.debug("Waiting for task to finish...");
+    await ecs.waitFor("tasksStopped", { cluster, tasks: [taskArn] }).promise();
+
+    core.debug("Checking status of task");
+    task = await ecs.describeTasks({ cluster, tasks: [taskArn] }).promise();
+    const exitCode = task.tasks[0].containers[0].exitCode;
+
+    if (exitCode === 0) {
+      core.setOutput("status", "success");
+    } else {
+      core.setFailed(task.tasks[0].stoppedReason);
+
+      const taskHash = taskArn.split("/").pop();
+      core.info(
+        `task failed, you can check the error on Amazon ECS console: https://console.aws.amazon.com/ecs/home?region=${AWS.config.region}#/clusters/${cluster}/tasks/${taskHash}/details`
+      );
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+main();
